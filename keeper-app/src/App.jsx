f65bd9008ddd2a7424967d8b658ee3da541b023a
SamniHasnath <samnihasnath@gmail.com>
2026-04-02 20:52:09 +0530

    add initial format

---
+++ keeper-app/src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Note from "./components/Note";
+import "./index.css";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Note />
+      <Note />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
